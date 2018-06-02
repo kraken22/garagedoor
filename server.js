8f846b1a5d4d40064961cfd1e6873ddc4ab29c97
Jordan Jantschulev <j.jantschulev@gmail.com>
2018-06-03 09:41:42 +1000

    added maxTimeout

--- server.js
+++ server.js
@@ -16,34 +16,44 @@ gpio.setup(PIN_NUMBER, gpio.DIR_OUT, function () {
 
 var socketNum = 0;
 
+var maxOnTime = 5000; //ms
+var maxOnTimeTimeout;
+
 io.on('connection', function (socket) {
   socketNum++;
   socket.socketNum = socketNum;
   console.log('Socket number ' + socket.socketNum + ' connected!');
-
   socket.emit('btnState', gpioState);
 
-  socket.on('doorBtnDown', function () {
-      if(!gpioState){
-        gpioState = true;
-        writeToPin(gpioState);
-        io.sockets.emit('btnState', gpioState);
-      }
-  });
+  socket.on('doorBtnDown', btnDown);
 
-  socket.on('doorBtnUp', function () {
-    if(gpioState){
-      gpioState = false;
-      writeToPin(gpioState);
-      io.sockets.emit('btnState', gpioState);
-    }
-  });
+  socket.on('doorBtnUp', btnUp);
 
   socket.on('disconnect', function () {
     console.log('Socket number ' + socket.socketNum + ' disconnected.');
   });
 
 });
+
+
+function btnDown() {
+  if(!gpioState){
+    gpioState = true;
+    writeToPin(gpioState);
+    io.sockets.emit('btnState', gpioState);
+    maxOnTimeTimeout = setTimeout(btnUp, maxOnTime);
+  }
+}
+
+
+function btnUp () {
+  if(gpioState){
+    gpioState = false;
+    writeToPin(gpioState);
+    io.sockets.emit('btnState', gpioState);
+    clearTimeout(maxOnTimeTimeout);
+  }
+}
 
 function writeToPin(on) {
   gpio.write(PIN_NUMBER, on, function (err) {
